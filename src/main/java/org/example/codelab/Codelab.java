8fb9e14c042e9b9994f60f3424a03751b70e052f
kyneedyouuu <rifkyrofiq01@gmail.com>
2024-02-25 16:45:57 +0700

    first commit

---
+++ src/main/java/org/example/codelab/Codelab.java
@@ -0,0 +1,35 @@
+package com.example.codelab;
+
+import java.time.Period;
+import java.util.Scanner;
+import java.time.LocalDate;
+
+
+public class Codelab {
+    public static void main(String[] args) {
+        String namaKamu;
+        String lakiAtauPerempuan;
+
+
+        Scanner objInput = new Scanner(System.in);
+
+        System.out.print("Nama : ");
+        namaKamu = objInput.nextLine();
+
+        System.out.print("Jenis Kelamin (L/P) : ");
+        lakiAtauPerempuan = objInput.nextLine();
+
+        System.out.print("Tanggal Lahir (yyyy-mm-dd) : ");
+        String tanggallahir = objInput.next();
+
+        LocalDate tanggal = LocalDate.parse(tanggallahir);
+        LocalDate hariIni = LocalDate.now();
+        Period agePeriod = Period.between(tanggal, hariIni);
+        int tahun = agePeriod.getYears();
+        int bulan = agePeriod.getMonths();
+
+        System.out.println("Nama : " + namaKamu);
+        System.out.println("Jenis Kelamin : " + lakiAtauPerempuan);
+        System.out.println("Umur Anda : " + tahun + " tahun " + bulan + "bulan");
+    }
+}
